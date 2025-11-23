784191474bc0cf22e2f180bea85e48c2ae21e9d0
abeerah2608-sudo <abeerah2608@gmail.com>
2025-11-23 10:08:13 +0500

    Enable error reporting and clean up signup.php

--- public/signup.php
+++ public/signup.php
@@ -1,6 +1,9 @@
 <?php
 require 'db_connect.php';
 header('Content-Type: application/json');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Only allow POST requests
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -62,3 +65,4 @@ if ($stmt->execute()) {
 
 $conn->close();
 ?>
+
