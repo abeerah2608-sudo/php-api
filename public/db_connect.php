c64afc0582bed68067b84ff9eb0493b0d933afd3
abeerah2608-sudo <abeerah2608@gmail.com>
2025-11-23 10:18:25 +0500

    Refactor db_connect.php to use environment variables

--- public/db_connect.php
+++ public/db_connect.php
@@ -1,15 +1,19 @@
 <?php
 // db_connect.php
-$servername = "hopper.proxy.rlwy.net";
-$username = "root";      // Default XAMPP username
-$password = "HEDEOrxqPLaJrHcQbLBtFvJPbcIjZEZA";          // Default XAMPP password is empty
-$dbname = "railway";
+
+$servername = getenv('DB_HOST') ?: 'localhost';
+$username   = getenv('DB_USER') ?: 'root';
+$password   = getenv('DB_PASS') ?: '';
+$dbname     = getenv('DB_NAME') ?: 'railway';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
+    die(json_encode([
+        "status" => "error",
+        "message" => "Connection failed: " . $conn->connect_error
+    ]));
 }
 ?>
